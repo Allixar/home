2373dbe7debac7245af2a451c462d9e53f60454c
Allen Chan <98492755+Allixar@users.noreply.github.com>
2025-07-04 10:02:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   const container = document.getElementById('main-container');
-  // It's a good practice to check if the element exists
   if (container) {
     container.style.opacity = 1;
     container.style.transform = 'translateY(0)';
@@ -28,19 +27,16 @@ function toggleProjectDetails(elementId) {
  * Handles switching between the 'Project Report' and 'Presentation' tabs.
  */
 function openTab(evt, tabName) {
-  // Get all elements with class="tab-content" and hide them
   const tabcontent = document.getElementsByClassName("tab-content");
   for (let i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tab-button" and remove the class "active"
   const tablinks = document.getElementsByClassName("tab-button");
   for (let i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
