2657cacc058cb30441ebc14822506c7d409c939a
Allen Chan <98492755+Allixar@users.noreply.github.com>
2025-07-04 08:10:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-window.onload = () => {
+window.addEventListener('DOMContentLoaded', (event) => {
   const container = document.getElementById('main-container');
-  container.style.opacity = 1;
-  container.style.transform = 'translateY(0)';
-};
+  // It's a good practice to check if the element exists
+  if (container) {
+    container.style.opacity = 1;
+    container.style.transform = 'translateY(0)';
+  }
+});
